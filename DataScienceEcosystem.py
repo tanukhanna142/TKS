79e42b90dd127a92ea14ea13ecfd833fd04d34ad
Tanu Khanna Sood <102407267+tanukhanna142@users.noreply.github.com>
2024-10-23 11:50:07 +0800

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #  Data Science Tools and Ecosystem
+
+# ### Data science tools & ecosystem summarized:
+# *The data science ecosystem comprises a variety of tools and platforms, including programming languages, platforms and cloud services*
+
+# Data science languages are programming languages widely used for data manipulation, analysis, and modeling. Key languagues include:
+# 
+# - Python
+# - R
+# - SQL
+# - Julia
+# - SAS
+
+# Data science libraries are specialized software packages that provide pre-built functions and tools for data manipulation, analysis and machine learning. Key libraries include:
+# 
+# - In Python: Pandas, NumPy, Matplotlib, Scikit-Learn
+# - In R: dplyr, ggplot2
+
+# Data Science Tools include:
+
+# | Data Science Tools   |
+# |----------------------|
+# | Jupyter Notebooks    |
+# | RStudio              |
+# | PyCharm              |
+# 
+
+# ### Below are a few examples of evaluating Arithmetic Expressions in Python
+# 
+# In Python, arithmetic expressions are used to perform calculations using operators like `+`, `-`, `*`, and `/`. Below are some examples:
+# 
+# - Addition: `5 + 3` results in `8`
+# - Subtraction: `10 - 4` results in `6`
+# - Multiplication: `7 * 2` results in `14`
+# - Division: `16 / 4` results in `4.0`
+# 
+
+# In[4]:
+
+
+# This is a simple arithmetic expression to multiply then add integers
+Result = (3 * 4) + 5
+print(Result)  # Expected output: 17
+
+
+# In[5]:
+
+
+# This will convert 200 minutes to hours by dividing by 60
+minutes = 200
+hours = minutes / 60
+print(hours)  # Expected output: 3.33
+
+
+# **Objectives:**
+# 
+# - List popular languages for Data Science
+# - Identify key libraries used in Data Science
+# - Understand commonly used tools in Data Science
+# - Introduction to Arithmetic Expressions with examples
+
+# ## Author
+# 
+# *Tanu Khanna Sood*
+# 
